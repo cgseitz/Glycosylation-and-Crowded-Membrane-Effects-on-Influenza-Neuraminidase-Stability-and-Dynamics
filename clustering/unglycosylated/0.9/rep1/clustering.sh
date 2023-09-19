f905abfd8b0ca77f5bd60b85b4584075e9ab52a1
Christian Seitz <30423859+cgseitz@users.noreply.github.com>
2023-09-19 10:52:25 -0700

    Add files via upload

---
+++ clustering/unglycosylated/0.9/rep1/clustering.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#run as ./clustering.sh
+
+module load cuda/11.3
+module load mdrun/2022.1-ubuntu
+module load gromacs/2022.1-ubuntu
+
+echo "3 0" | gmx_mpi cluster -f unglycosylated_rep1.trr -s subset.pdb -g clustering.log -dist clusteringdistance.xvg -sz clusteringsize.xvg -cl finalclusters.pdb -cutoff 0.09 -method gromos
+
+sh frequency.sh
+
+rm \#*
